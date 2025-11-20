99457ad6a0949cd02c48be051b66f9c445a93b88
Erick Silva <erick_novo@hotmail.com>
2025-11-20 12:23:38 -0300

    data handling; multi-hot vectors, scaling numerical features

--- data_handling/cleaning.py
+++ data_handling/cleaning.py
@@ -3,19 +3,67 @@ import re
 import html
 from dotenv import load_dotenv
 import os
+from collections import Counter
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 
 class DataCleaner:
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, add_numerical: bool = False):
         self.df = pd.read_parquet(filename)
+        self.add_numerical = add_numerical
 
-        self.cols_to_exclude = ['item_id', 'episodes', 'format', 'average_score', 'popularity', 'image_url', 'studios', 'season_year', 'source']
+    def final_stage_df(self):
+        self._first_stage_df()
+        self._second_stage_df()
 
-    def first_stage_df(self):
+        cols = ['text', 'genres_multi', 'tags_multi', 'studios_multi']
+        if self.add_numerical:
+            cols += ['episodes', 'popularity']
+        return self.df[cols]
+
+    # transform values into multi-hot vectors; multi-hot vectors are more useful when dealing with neural networks later
+    def _second_stage_df(self):
+        cols_to_multihot = ['genres', 'tags', 'studios']
+        for col in cols_to_multihot:
+            all_values = Counter()
+            self.df[f"{col}_list"] = self.df[col].apply(self._split_vals)
+            for v_list in self.df[f"{col}_list"]:
+                all_values.update(v_list)
+            col2id = {c: i for i, (c, _) in enumerate(all_values.most_common())}
+            num_values = len(col2id)
+
+            self.df[f'{col}_multi'] = self.df[f'{col}_list'].apply(lambda x: self._col_to_multihot(x, num_values, col2id))
+
+    @staticmethod
+    def _col_to_multihot(vlist, num_values, col2id):
+        arr = np.zeros(num_values, dtype=int)
+        for g in vlist:
+            if g in col2id:
+                arr[col2id[g]] = 1
+        return arr
+
+    @staticmethod
+    def _split_vals(v):
+        if pd.isna(v) or v == "":
+            return []
+        if isinstance(v, list):
+            return v
+        return str(v).split("|")
+
+    # clean dataframe and modify numerical and categorical features
+    def _first_stage_df(self):
         self.df = self.df.drop_duplicates(subset="item_id")
         self.df = self.df.dropna(subset=['title'])
         self.df["synopsis"] = self.df["synopsis"].fillna("")
-        self.df = self.df.drop(self.cols_to_exclude, axis=1)
+
+        if self.add_numerical:
+            num_cols = ["popularity", "episodes"]
+            for c in num_cols:
+                self.df[c] = pd.to_numeric(self.df[c], errors="coerce").fillna(0)
+
+            scaler = MinMaxScaler()
+            self.df[num_cols] = scaler.fit_transform(self.df[num_cols])
 
         self.df["text"] = self.df.apply(self._make_text, axis=1)
         self.df["text"] = self.df["text"].astype(str).map(self._clean_text)
@@ -48,10 +96,13 @@ class DataCleaner:
         return s
 
 
+
+""" test """
+
 load_dotenv()
 
-dc = DataCleaner(os.getenv('PARQUET_FILENAME'))
-dc.first_stage_df()
+dc = DataCleaner(os.getenv('PARQUET_FILENAME'), add_numerical=True)
+df = dc.final_stage_df()
 
 
 print()
